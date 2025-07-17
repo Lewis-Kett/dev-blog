cdb625946a3fc5056e77ad557697c3ce9d9b47da
Lewis_Kett <lewis.a.kett@gmail.com>
2025-07-17 15:22:38 +0100

    adds mvp feedback carousel

--- src/components/custom/carousel/feedback-carousel.tsx
+++ src/components/custom/carousel/feedback-carousel.tsx
@@ -1,18 +1,44 @@
+'use client'
+
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 import { FeedbackCard } from '../card'
 import { feedbackCarouselContent } from '@/content'
+import AutoPlay from 'embla-carousel-autoplay'
 
 const FeedbackCarousel = () => {
   return (
-    <section>
+    <section className="mx-auto md:max-w-4/5 xl:max-w-2/3 mb-36">
       <div className="px-5 pt-24 pb-12">
-        <h1 className="text-3xl md:text-5xl">Expert Feedback</h1>
+        <h1 className="pb-5 text-3xl md:text-5xl">Expert Feedback</h1>
         <p className="text-lg">
           Read insights and praises from mentors and peers who have guided and witnessed my professional growth.
         </p>
       </div>
-      {feedbackCarouselContent.map(({ name, title, company, comments }) => (
-        <FeedbackCard key={name} name={name} title={title} company={company} comments={comments} />
-      ))}
+      <div className="mx-5">
+        <Carousel
+          className="flex items-center justify-center"
+          opts={{
+            align: 'start',
+            loop: true,
+          }}
+          plugins={[
+            AutoPlay({
+              delay: 6000,
+              stopOnInteraction: true,
+            }),
+          ]}
+        >
+          <CarouselContent>
+            {feedbackCarouselContent.map(({ name, title, company, comments }, i) => (
+              <CarouselItem key={name + i} className="md:basis-1/2 lg:basis-1/3">
+                <FeedbackCard name={name} title={title} company={company} comments={comments} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="hidden md:flex" />
+          <CarouselNext className="hidden md:flex" />
+        </Carousel>
+      </div>
     </section>
   )
 }
--- src/content/feedback-carousel-content.ts
+++ src/content/feedback-carousel-content.ts
@@ -2,7 +2,31 @@ export const feedbackCarouselContent = [
     {
       name: 'Stevie Mccann',
       image: 'Stevie.jpg',
-      title: 'AI Principal Developer',
+      title: 'AI Principal Software Engineer',
+      company: 'Booking.com',
+      comments:
+        'Lewis has an extraordinary talent for translating complex concepts into visually appealing and intuitive designs. Working alongside him as a mentor, I was impressed by his ability to seamlessly integrate feedback and push the boundaries of whats possible in digital design.',
+    },
+    {
+      name: 'Stevie Mccann',
+      image: 'Stevie.jpg',
+      title: 'AI Principal Software Engineer',
+      company: 'Booking.com',
+      comments:
+        'Lewis has an extraordinary talent for translating complex concepts into visually appealing and intuitive designs. Working alongside him as a mentor, I was impressed by his ability to seamlessly integrate feedback and push the boundaries of whats possible in digital design.',
+    },
+    {
+      name: 'Stevie Mccann',
+      image: 'Stevie.jpg',
+      title: 'AI Principal Software Engineer',
+      company: 'Booking.com',
+      comments:
+        'Lewis has an extraordinary talent for translating complex concepts into visually appealing and intuitive designs. Working alongside him as a mentor, I was impressed by his ability to seamlessly integrate feedback and push the boundaries of whats possible in digital design.',
+    },
+    {
+      name: 'Stevie Mccann',
+      image: 'Stevie.jpg',
+      title: 'AI Principal Software Engineer',
       company: 'Booking.com',
       comments:
         'Lewis has an extraordinary talent for translating complex concepts into visually appealing and intuitive designs. Working alongside him as a mentor, I was impressed by his ability to seamlessly integrate feedback and push the boundaries of whats possible in digital design.',
